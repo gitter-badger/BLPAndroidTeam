ff5743ea07fb277e2702376aad82ad2d026309c1
unknown <212464344@USSYFSTML162818.adsyf.syfbank.com>
2015-12-11 13:48:42 -0500

    crispy login

--- app/src/main/java/com/syfblp/sas/blpappv2/Login.java
+++ app/src/main/java/com/syfblp/sas/blpappv2/Login.java
@@ -8,14 +8,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     EditText editText;
     Button button;
     String password;
+    String password2;
+    String CRISPY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,36 +28,62 @@ public class Login extends AppCompatActivity {
         editText=(EditText)findViewById(R.id.editText2);
         button=(Button) findViewById(R.id.button2);
         password="AndroidTeam";
+        password2 = "IOS";
+        CRISPY = "CRISPY";
 
     }
 
     public void Success(View view) {
-        String entered =editText.getText().toString();
-        if (entered.equals(password)){
-            Intent intent= new Intent(this,MainActivity.class);
+
+        String entered = editText.getText().toString();
+        if (entered.equals(password)) {
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        } else {
+            if (entered.equals(password2)) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setMessage("not " + CRISPY + " enough to enter");
+                alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+                editText.setText("");
+            }
+            else{
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setMessage("Please enter the correct password");
+
+                alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+                editText.setText("");
+            }
         }
-        else{
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setMessage("Please enter the correct password");
 
-            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface arg0, int arg1) {
-
-                }
-            });
-
-            alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
-            editText.setText("");
         }
-    }
+
 }
